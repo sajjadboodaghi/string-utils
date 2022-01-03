380ce290c86a077d3b82a0fa00033ec28988974b
sajjadboodaghi <sajjad.boodaghi@gmail.com>
2022-01-03 15:10:27 +0330

    Add files via upload

---
+++ src/Str.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace QueraCollege\StringUtils;
+
+class Str
+{
+
+    /**
+     * Determines if a given string contains a given substring
+     *
+     * @param   string  $haystack
+     * @param   string|string[] $needles
+     * @return  bool
+     */
+    public static function contains(string $haystack, $needles): bool {
+        foreach((array) $needles as $needle) {
+            if($needle != '' && mb_strpos($haystack, $needle) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Determines if a given string contains all array values
+     *
+     * @param   string $haystack
+     * @param   string[] $needles
+     * @return  bool
+     */
+    public static function containsAll(String $haystack, array $needles): Bool {
+        foreach($needles as $needle) {
+            if(!static::contains($haystack, $needle)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
